6218bb83e9111cd637dbc177f9fa0ff62827b94d
hotsaucejake <get@hotsaucejake.com>
2018-01-08 23:58:03 +0000

    initial vue changes
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- resources/assets/js/components/BotManTinker.vue
+++ resources/assets/js/components/BotManTinker.vue
@@ -2,35 +2,128 @@
     <div>
         <div class="arrow"></div>
         <ul class="ChatLog">
-            <li class="ChatLog__entry" v-for="message in messages" :class="{'ChatLog__entry_mine': message.isMine}">
+            <li class="ChatLog__entry" v-for="(message, index) in messages" :key="index" :class="[{'ChatLog__entry_mine': message.isMine}, {'ChatLog__entry_button':message.buttons.length}]">
                 <img class="ChatLog__avatar" src="/logo.png" />
-                <p class="ChatLog__message">{{ message.text }}</p>
+                <p class="ChatLog__message" v-if="!message.buttons.length">{{ message.text }}</p>
+                <p class="ChatLog__message" v-else-if="message.buttons.length"><a :href="button.url" class="chatButton" @click="button.payload ? sendMessage(button.payload) : null" v-for="button in message.buttons">{{ button.text }}</a></p>
             </li>
         </ul>
 
-        <input type="text" class="ChatInput" @keyup.enter="sendMessage" v-model="newMessage" placeholder="">
+        <input type="text" class="ChatInput" @keyup.enter="sendMessage" v-model="newMessage" placeholder="Type a message">
     </div>
 </template>
 
-<style>
+<script>
+    const axios = require('axios');
+    const API_ENDPOINT = '/botman';
+
+    export default {
+        data() {
+            return {
+                messages: [],
+                newMessage: null
+            };
+        },
+
+        methods: {
+            _addMessage(text, attachment, isMine, buttons=[]) {
+                this.messages.push({
+                    'isMine': isMine,
+                    'user': isMine ? '👨' : '🤖',
+                    'text': text,
+                    'attachment': attachment || {},
+                    'buttons': buttons,
+                });
+            },
+
+            sendMessage(msg=null) {
+                let messageText = '';
+
+                if (typeof(msg) !== "string") {
+                  messageText = this.newMessage;
+                }
+                else {
+                  messageText = msg;
+                }
+                this.newMessage = '';
+                if (messageText === 'clear') {
+                    this.messages = [];
+                    return;
+                }
+
+                this._addMessage(messageText, null, true);
+
+                axios.post(API_ENDPOINT, {
+                    driver: 'web',
+                    userId: 9999999,
+                    message: messageText
+                }).then(response => {
+                    let messages = response.data.messages || [];
+                    messages.forEach(msg => {
+                        this._addMessage(msg.text, msg.attachment, false);
+                        if (typeof(msg.actions) !== 'undefined') {
+                          let actions = msg.actions || [];
+                          let buttons = [];
+                          actions.forEach(act => {
+                            if (act.type === 'button') {
+                              buttons.push({'text':act.text, 'payload':act.text, 'url':'#'});
+                            }
+
+                          });
+                          this._addMessage('', {}, false, buttons);
+
+                        }
+                        if (msg.type === 'buttons') {
+                          //get all buttons
+                          let actions = msg.buttons || [];
+                          let buttons = [];
+                          actions.forEach(act => {
+                            if (act.type === 'postback') {
+                              buttons.push({'text':act.title, 'payload':act.payload, 'url':'#'});
+                            }
+                            else if (act.type === 'web_url') {
+                              buttons.push({'text':act.title, 'payload':'', 'url':act.url});
+                            }
+
+                          });
+                          this._addMessage('', {}, false, buttons);
+
+                        }
+                    });
+                }, response => {
+
+                });
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
     input.ChatInput {
         width: 300px;
         height: 25px;
         border-radius: 5px;
         border: none;
         padding: 10px;
+        margin-bottom: 10px;
+
+        &:focus {
+            outline: none;
+        }
     }
 
     ul.ChatLog {
         list-style: none;
+        padding: 0;
     }
 
     .ChatLog {
         max-width: 20em;
         margin: 0 auto;
-    }
-    .ChatLog .ChatLog__entry {
-        margin: .5em;
+
+        .ChatLog__entry {
+            margin: .5em;
+        }
     }
 
     .ChatLog__entry {
@@ -42,6 +135,16 @@
 
     .ChatLog__entry.ChatLog__entry_mine {
         flex-direction: row-reverse;
+
+        .ChatLog__message {
+            &:before {
+                right: -12px;
+                bottom: .6em;
+                left: auto;
+                border: 6px solid transparent;
+                border-left-color: #08f;
+            }
+        }
     }
 
     .ChatLog__avatar {
@@ -55,32 +158,39 @@
     }
 
     .ChatLog__entry.ChatLog__entry_mine
+    .ChatLog__avatar,
+    .ChatLog__entry.ChatLog__entry_button
     .ChatLog__avatar {
         display: none;
+    }
+
+    .ChatLog__entry_button .ChatLog__message {
+        min-width: 100%;
+        left:-20px;
     }
 
     .ChatLog__entry .ChatLog__message {
         position: relative;
         margin: 0 12px;
+
+        &:before {
+            position: absolute;
+            right: auto;
+            bottom: .6em;
+            left: -12px;
+            height: 0;
+            content: '';
+            border: 6px solid transparent;
+            border-right-color: #ddd;
+            z-index: 2;
+        }
     }
 
-    .ChatLog__entry .ChatLog__message::before {
-        position: absolute;
-        right: auto;
-        bottom: .6em;
-        left: -12px;
-        height: 0;
-        content: '';
-        border: 6px solid transparent;
-        border-right-color: #ddd;
-        z-index: 2;
-    }
-
-    .ChatLog__entry.ChatLog__entry_mine .ChatLog__message::before {
+    .ChatLog__entry.ChatLog__entry_button .ChatLog__message::before {
         right: -12px;
         bottom: .6em;
         left: auto;
-        border: 6px solid transparent;
+        border: none;
         border-left-color: #08f;
     }
 
@@ -98,53 +208,15 @@
         background-color: #08f;
         color: #fff;
     }
-</style>
 
-<script>
-    const axios = require('axios');
-    const API_ENDPOINT = '/botman';
-
-    export default {
-        data() {
-            return {
-                messages: [],
-                newMessage: null
-            };
-        },
-
-        methods: {
-            _addMessage(text, attachment, isMine) {
-                this.messages.push({
-                    'isMine': isMine,
-                    'user': isMine ? '👨' : '🤖',
-                    'text': text,
-                    'attachment': attachment || {},
-                });
-            },
-
-            sendMessage() {
-                let messageText = this.newMessage;
-                this.newMessage = '';
-                if (messageText === 'clear') {
-                    this.messages = [];
-                    return;
-                }
-
-                this._addMessage(messageText, null, true);
-
-                axios.post(API_ENDPOINT, {
-                    driver: 'web',
-                    userId: 9999999,
-                    message: messageText
-                }).then(response => {
-                    let messages = response.data.messages || [];
-                    messages.forEach(msg => {
-                        this._addMessage(msg.text, msg.attachment, false);
-                    });
-                }, response => {
-
-                });
-            }
-        }
+    a.chatButton{
+     background-color:#5cb85c;
+     border:1px solid #4cae4c;
+     border-radius:5px;
+     color:#fff;
+     margin:2px 10px 2px 0;
+     padding:5px 10px 5px 10px;
+     text-decoration:none;
+     display: inline-block;
     }
-</script>
+</style>
--- resources/views/tinker.blade.php
+++ resources/views/tinker.blade.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>BotMan Studio</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Cora | Corus360 Chatbot</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
@@ -16,7 +18,7 @@
             font-family: "Source Sans Pro", sans-serif;
             margin: 0;
             padding: 0;
-            background: radial-gradient(#57bfc7, #45a6b3);
+            background: radial-gradient(#fff, #f99);
         }
 
         .container {
