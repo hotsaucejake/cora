774279b9fa6edd534dc2293aa4cd1af27fb79d71
hotsaucejake <get@hotsaucejake.com>
2018-01-10 07:26:17 +0000

    integrate dialogflow
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- routes/botman.php
+++ routes/botman.php
@@ -1,7 +1,24 @@
 <?php
 use App\Http\Controllers\BotManController;
+use BotMan\BotMan\Middleware\ApiAi;
+
+$dialogflow = ApiAi::create(env('DF_CORA_CLIENT'))->listenForAction();
 
 $botman = resolve('botman');
+
+// Apply global "received" middleware
+$botman->middleware->received($dialogflow);
+
+// Apply matching middleware per hears command
+$botman->hears('smalltalk.agent.acquaintance', function ($bot) { // The incoming message matched the action on Dialogflow
+    // Retrieve Dialogflow information:
+    $extras = $bot->getMessage()->getExtras();
+    $apiReply = $extras['apiReply'];
+    $apiAction = $extras['apiAction'];
+    $apiIntent = $extras['apiIntent'];
+    
+    $bot->reply($apiReply);
+})->middleware($dialogflow);
 
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
@@ -9,5 +26,7 @@ $botman->hears('Hi', function ($bot) {
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
 
 $botman->fallback(function($bot) {
+    // logic to save conversation
+    // logic to notify
     $bot->reply('I didn\'t quite understand you... no worries :) I saved this convo and will ask on your behalf, I\'m new at this job and still learning!');
 });
