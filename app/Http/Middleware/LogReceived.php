b4be33c67ea2eafa96596e680b4edd20a898ff5a
hotsaucejake <get@hotsaucejake.com>
2018-01-31 20:07:03 +0000

    log received nexmo exchanges
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- app/Http/Middleware/LogReceived.php
+++ app/Http/Middleware/LogReceived.php
@@ -6,6 +6,13 @@ use Botman\Botman\Botman;
 use BotMan\BotMan\Interfaces\Middleware\Received;
 use BotMan\BotMan\Messages\Incoming\IncomingMessage;
 
+// drivers to check for
+use BotMan\Drivers\Nexmo\NexmoDriver;
+use BotMan\Drivers\BotFramework\BotFrameworkDriver;
+
+// save exchanges
+use App\NexmoExchange;
+
 use Illuminate\Support\Facades\Log;
 
 class LogReceived implements Received
@@ -21,7 +28,32 @@ class LogReceived implements Received
      */
     public function received(IncomingMessage $message, $next, BotMan $bot)
     {
-        Log::debug(print_r($message, true));
+        if($bot->getDriver() instanceof NexmoDriver) {
+
+            $payload = $message->getPayload(); // get the message payload (array)
+            $exchange = new NexmoExchange; // store exchange info
+            
+            $exchange->msisdn = $payload['msisdn'];
+            $exchange->to = $payload['to'];
+            $exchange->message_id = $payload['messageId'];
+            $exchange->text = $payload['text'];
+            $exchange->type = $payload['type'];
+            $exchange->keyword = $payload['keyword'];
+            $exchange->message_timestamp = $payload['message-timestamp'];
+
+            $exchange->save();
+
+        } elseif($bot->getDriver() instanceof BotFrameworkDriver){
+
+            Log::info('BotFrameworkDriver');
+
+        } else {
+
+            Log::info(print_r($bot->getDriver(), true));
+            
+        }
+
+        // Log::debug(print_r($bot, true));  // exceeds memory size
 
         return $next($message);
     }
