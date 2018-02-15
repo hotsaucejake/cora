ec409697524cd83e4fd6301888cef0d17c16b012
hotsaucejake <get@hotsaucejake.com>
2018-02-15 20:51:34 +0000

    users migration
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

---
+++ database/migrations/2018_02_15_204627_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');                   // name will be pulled from azure
+            $table->string('email')->unique();        // email will be pulled from azure
+            $table->string('password')->nullable();   // users can later set a password in case of azure failure
+            $table->string('azure_id')->unique();     // initial registration
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
