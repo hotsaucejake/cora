ec78f09c5fd50b789d60f300235a85e9973b2d11
hotsaucejake <get@hotsaucejake.com>
2018-02-21 00:40:03 +0000

    add head-title yield and section
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.monster.app')
 
+@section('head-title')
+    Cora | Dashboard
+@endsection
+
 @section('content')
     @include('layouts.monster.partials.example-content')
 @endsection
--- resources/views/layouts/monster/partials/head.blade.php
+++ resources/views/layouts/monster/partials/head.blade.php
@@ -6,8 +6,8 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="/monster/assets/images/favicon.png">
-    <title>Cora</title>
+    <link rel="shortcut icon" href="/favicon.ico">
+    <title> @yield('head-title') </title>
     <!-- Bootstrap Core CSS -->
     <link href="/monster/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- chartist CSS -->
@@ -29,18 +29,4 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
-<script>
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-    ga('create', 'UA-85622565-1', 'auto');
-    ga('send', 'pageview');
-    </script>
 </head>
