2d6203b8d8af56b68a23a782512951fc15ba8cee
hotsaucejake <get@hotsaucejake.com>
2018-02-21 05:58:49 +0000

    redirect to dashboard
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
@@ -57,7 +57,7 @@ class LoginController extends Controller
          }
          $authUser = $this->findOrCreateUser($azureUser);
          Auth::login($authUser, true);
-         return redirect()->intended('home');
+         return redirect()->intended('dashboard');
     }
 
     
