3787b0cb8d2d1c938a641fbe632562854597c2e8
hotsaucejake <get@hotsaucejake.com>
2018-02-21 05:10:47 +0000

    change default config for avatar
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- config/laravolt/avatar.php
+++ config/laravolt/avatar.php
@@ -25,19 +25,19 @@ return [
     'ascii'    => false,
 
     // Image shape: circle or square
-    'shape' => 'circle',
+    'shape' => 'square',
 
     // Image width, in pixel
-    'width'    => 100,
+    'width'    => 300,
 
     // Image height, in pixel
-    'height'   => 100,
+    'height'   => 300,
 
     // Number of characters used as initials. If name consists of single word, the first N character will be used
     'chars'    => 2,
 
     // font size
-    'fontSize' => 48,
+    'fontSize' => 144,
 
     // convert initial letter in uppercase
     'uppercase' => false,
